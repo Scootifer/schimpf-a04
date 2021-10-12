ff09a124874038323956824c5f4b73fd9c3413e0
Scootifer <Windmill3341!>
2021-10-12 19:06:58 -0400

    Solution42 skeleton

---
+++ exercise42/src/main/java/FileParser.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Scott Schimpf
+ */
+
+//This class will use a readCSV function to read the CSV file
+// The data will be stored in its respective arraylist
+// The class will use a printTable function to print the data
+// For junit testing a getter method will be made for each arrayList
+
+public class FileParser {
+
+
+
+}
---
+++ exercise42/src/main/java/Solution42.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Scott Schimpf
+ */
+
+
+//THis program will use a FileParser class that will handle reading, storing, and outputting data
+
+public class Solution42 {
+
+}
