c7f80e2d02cf9cb50bb3eda082fd4cf3351964a0
Scootifer <Windmill3341!>
2021-10-12 16:11:15 -0400

    final sol41

--- exercise41/src/main/java/Solution41.java
+++ exercise41/src/main/java/Solution41.java
@@ -3,7 +3,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Collections;
 
 //This program will use the sortNames(String fileName) function to read the file to a list and then use the collections package sort feature.
 // Since all names have a comma in them it is fair to count it in the calculation as the collections package does.
@@ -35,7 +34,7 @@ class Solution41 {
             writer.close();
         }
         catch(Exception e){
-            System.out.println("Error");
+            System.out.println("Error " + e);
         }
 
 
--- exercise41/src/test/java/TestClass.java
+++ exercise41/src/test/java/TestClass.java
@@ -1,10 +1,19 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TestClass {
 
     @Test
-    void showSimpleAssertion() {
+    void showSimpleAssertion() throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("exercise41_output.txt"));
         Solution41 sol = new Solution41();
+        String[] correct = {"Frank", "Lisa", "Tom"};
+        for(String i : correct) {
+            Assertions.assertTrue(i.equals(scan.nextLine()));
+        }
     }
 }
