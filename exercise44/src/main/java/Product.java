5bf7ad98529682d1886c9b935274befdb5cfc17f
Scootifer <Windmill3341!>
2021-10-16 14:11:38 -0400

    Solution44 completed

--- exercise44/src/main/java/Product.java
+++ exercise44/src/main/java/Product.java
@@ -3,11 +3,44 @@
  *  Copyright 2021 Scott Schimpf
  */
 
-// The product class will only consist of a name, price, and quantity variable, and the according
+// The Product class will only consist of a name, price, and quantity variable, and the according
 // Constructor
 
 
 public class Product {
 
+    private String name;
+    private double price;
+    private int quantity;
 
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String n){
+        this.name = n;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double p){
+        this.price = p;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int q){
+        this.quantity = q;
+    }
 }
--- exercise44/src/main/java/Solution44.java
+++ exercise44/src/main/java/Solution44.java
@@ -6,5 +6,84 @@
 //This program will use the gson library to parse the json file. I will make use of the
 // gson library's ability to be written into a class Product
 
+//updated psuedocode
+//function inputLoop will handle input and return a valid string name
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Solution44 {
+
+    private List<Product> productList = new ArrayList<>();
+    private Scanner scan = new Scanner(System.in);
+
+    void printProduct(Product p){
+        System.out.println("Name: " + p.getName());
+        System.out.println("Price: " + p.getPrice());
+        System.out.println("Quantity: " + p.getQuantity());
+    }
+
+    //returns 0 when found
+    //returns -1 for not found
+    int findProduct(String name) {
+        for(Product p : this.productList) {
+            if( p.getName().equalsIgnoreCase(name)){
+                printProduct(p);
+                return 0;
+            }
+        }
+        return -1;
+    }
+
+    void inputLoop(){
+        int wordFound;
+
+        do {
+            System.out.print("Please enter a Product: ");
+            wordFound = findProduct(this.scan.nextLine());
+
+            if (wordFound == 0) {
+                return;
+            }
+            else {
+                System.out.println("Sorry that Product was not found");
+            }
+
+        }while(true);
+    }
+
+    void readJson(String filename) {
+        try{
+            Gson gson = new Gson();
+
+            Reader reader = Files.newBufferedReader(Paths.get(filename));
+            JsonObject e = (JsonObject) JsonParser.parseReader(reader);
+            JsonArray list = e.getAsJsonArray("products");
+
+
+            productList = new Gson().fromJson(list, new TypeToken<List<Product>>() {}.getType());
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Solution44 obj = new Solution44();
+        obj.readJson("exercise44_input.json");
+        obj.inputLoop();
+
+
+
+
+
+    }
 }
