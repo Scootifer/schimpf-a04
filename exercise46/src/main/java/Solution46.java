4aee5ade73ed812470e750c36b833fbd8d751e32
Scootifer <Windmill3341!>
2021-10-16 17:39:42 -0400

    exercise46 skeleton

---
+++ exercise46/src/main/java/Solution46.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Scott Schimpf
+ */
+
+//Similarly to the last solution, this solution will store a list containing each of the lines in the file
+// The solution will then loop through each line word by word, comparing it to values in a treemap
+// if any keys match, the value will be incremented, if the value is not found it will be added
+// Making use of a tree map comes with automatic sorting
+
+public class Solution46 {
+
+
+}
