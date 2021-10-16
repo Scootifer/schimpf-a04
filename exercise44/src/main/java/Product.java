b68a043f0bc44385becb6a8c1278b27b47092d46
Scootifer <Windmill3341!>
2021-10-16 12:45:20 -0400

    exercise 44 Skeleton

---
+++ exercise44/src/main/java/Product.java
@@ -0,0 +1,13 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Scott Schimpf
+ */
+
+// The product class will only consist of a name, price, and quantity variable, and the according
+// Constructor
+
+
+public class Product {
+
+
+}
---
+++ exercise44/src/main/java/Solution44.java
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Scott Schimpf
+ */
+
+//This program will use the gson library to parse the json file. I will make use of the
+// gson library's ability to be written into a class Product
+
+public class Solution44 {
+}
