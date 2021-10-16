08bc4395782b20773959676eb367ed46822c86df
Scootifer <Windmill3341!>
2021-10-16 14:41:04 -0400

    SOlution45 skeleton

---
+++ Solution45/src/main/java/FileHandler.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Scott Schimpf
+ */
+
+//This class will read in the file using a scanner
+// The program will use the split function to split the input into a list of string lists
+//each list will be the corresponding line in the file
+
+
+public class FileHandler {
+}
---
+++ Solution45/src/main/java/Solution45.java
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Scott Schimpf
+ */
+
+// the solution class will only handle input of the output file name
+
+
+public class Solution45 {
+}
